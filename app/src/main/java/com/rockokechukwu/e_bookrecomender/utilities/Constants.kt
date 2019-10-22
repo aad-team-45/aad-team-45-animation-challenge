e001dcd6ace550c9eb719ef5d106cbff99407b75
dave-oh-git <hirakoshinji722@gmail.com>
2019-10-22 11:32:58 +0100

    +++ ability to add api key via local.properties file(assign key to API_KEY variable ).

--- app/src/main/java/com/rockokechukwu/e_bookrecomender/utilities/Constants.kt
+++ app/src/main/java/com/rockokechukwu/e_bookrecomender/utilities/Constants.kt
@@ -1,6 +1,8 @@
 package com.rockokechukwu.e_bookrecomender.utilities
 
+import com.rockokechukwu.e_bookrecomender.BuildConfig
+
 const val API_SERVICE_AUTHENTICATION_NAME = "key"
-const val API_SERVICE_AUTHENTICATION_KEY = "" // TODO: Use your API Key
+const val API_SERVICE_AUTHENTICATION_KEY = BuildConfig.API_KEY
 const val FILTER_QUERY = "Ebook"
 const val SEPARATOR = "-"
