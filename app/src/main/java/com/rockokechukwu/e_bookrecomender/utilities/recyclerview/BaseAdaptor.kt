ac48b1f7626bd0ea386eda169ee22cddb366a71b
dave-oh-git <hirakoshinji722@gmail.com>
2019-10-25 07:20:06 +0100

    +++ app wide base adaptor for recyclerview

--- app/src/main/java/com/rockokechukwu/e_bookrecomender/utilities/recyclerview/BaseAdaptor.kt
+++ app/src/main/java/com/rockokechukwu/e_bookrecomender/utilities/recyclerview/BaseAdaptor.kt
@@ -1,6 +1,36 @@
 package com.rockokechukwu.e_bookrecomender.utilities.recyclerview
 
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.databinding.ViewDataBinding
 import androidx.recyclerview.widget.RecyclerView
 
-class BaseAdaptor {
+abstract class BaseAdaptor: RecyclerView.Adapter<AppViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): AppViewHolder {
+        val layoutInflater = LayoutInflater.from(parent.context)
+
+        val binding = DataBindingUtil.inflate<ViewDataBinding>(layoutInflater, viewType,
+             parent, false )
+
+        return AppViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: AppViewHolder, position: Int) {
+
+        val obj = getObjForPosition(position)
+        holder.bind(obj)
+
+    }
+
+    override fun getItemViewType(position: Int) = getLayoutIdForPosition(position)
+
+
+    abstract fun getObjForPosition(position: Int)
+
+    /*generate layout ID for position, so it support different layout types depending on position*/
+    abstract fun getLayoutIdForPosition(position: Int): Int
+
+
 }
